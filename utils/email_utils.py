cae93232cfc68e453e2c8452cf4045a7718ae07f
johnnas12 <yohanesmesfin3@gmail.com>
2025-05-06 09:25:26 +0300

    chore: Email configurations for daily digest

---
+++ utils/email_utils.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.message import EmailMessage
+from dotenv import load_dotenv
+import os
+
+# Load env vars
+load_dotenv()
+
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+def send_email(subject, content, recipient_email):
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = recipient_email
+    msg.set_content(content)
+
+    try:
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+            smtp.send_message(msg)
+        print(f"✅ Email sent to {recipient_email}")
+    except Exception as e:
+        print("❌ Error sending email:", e)
